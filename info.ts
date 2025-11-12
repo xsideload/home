413e3ef9311497a37c789239a2ce5b0ad92b4f7b
gx8z <86722990+gx8z@users.noreply.github.com>
2025-11-12 17:30:18 +0700

    add info and info types, helpers

---
+++ info.ts
@@ -0,0 +1,30 @@
+import { defineBase } from "./scripts/lib/infoTypes";
+
+export const assetsRepo = { owner: "xsideload", repo: "assets-test" } as const;
+
+export const base = defineBase({
+	YouTube: {
+		bundleIdentifier: "com.google.ios.youtube",
+		tweaks: {
+			YTLite: {
+				actionRepo: {
+					owner: "xsideload",
+					repo: "YTLite",
+					basehead: "main...dayanch96:YTLite:main"
+				}
+			}
+		}
+	},
+	YouTubeMusic: {
+		bundleIdentifier: "com.google.ios.youtubemusic",
+		tweaks: {
+			YTMusicUltimate: {
+				actionRepo: {
+					owner: "xsideload",
+					repo: "YTMusicUltimate",
+					basehead: "main...dayanch96:YTMusicUltimate:main"
+				}
+			}
+		}
+	}
+});
---
+++ scripts/lib/info.ts
@@ -0,0 +1,42 @@
+import type { AppInfo, TweakInfo } from "@/scripts/lib/infoTypes";
+import { base } from "@/info";
+
+export { assetsRepo } from "@/info";
+
+export type AppName = keyof typeof base;
+export type TweakName = {
+  [K in AppName]: (typeof base)[K] extends { readonly tweaks: any }
+  ? keyof (typeof base)[K]["tweaks"]
+  : never;
+}[AppName];
+
+export const apps = new Map<AppName, AppInfo>();
+export const tweaks = new Map<TweakName, TweakInfo>();
+const tweakNameToAppName = new Map<TweakName, AppName>();
+
+for (const [appName, appData] of Object.entries(base) as [
+  AppName,
+  (typeof base)[AppName]
+][]) {
+  const { tweaks: appTweaks, ...appInfo } = appData;
+  apps.set(appName, appInfo);
+  if (appTweaks) {
+    for (const [tweakName, tweakData] of Object.entries(appTweaks) as [
+      TweakName,
+      TweakInfo
+    ][]) {
+      tweaks.set(tweakName, tweakData);
+      tweakNameToAppName.set(tweakName, appName);
+    }
+  }
+}
+
+export const getAppInfoFromTweakName = (tweakName: TweakName) => {
+  const appName = tweakNameToAppName.get(tweakName);
+  if (appName) {
+    return {
+      name: appName,
+      ...apps.get(appName)
+    };
+  }
+};
